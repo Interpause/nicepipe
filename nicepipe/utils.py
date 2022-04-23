e4844ba17edcf8a7c1b835aa0089707e1d28c96e
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2022-04-23 23:41:08 +0800

    support async iterators as well

--- nicepipe/utils.py
+++ nicepipe/utils.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import time
 import asyncio
+from typing import AsyncIterable
 from numpy import ndarray
 from cv2 import imencode
 from base64 import b64encode
@@ -10,10 +11,18 @@ async def rlloop(rate, iterator=None, update_func=lambda: 0):
     '''rate-limited loop in Hz. Loop is infinite if iterator is None.'''
     t = time.perf_counter()
 
-    # set iterator to be infinite if None
-    iterator = iter(int, 1) if iterator is None else iterator
+    is_async = isinstance(iterator, AsyncIterable)
+    if is_async:
+        iterator = iterator.__aiter__()
+    else:
+        # set iterator to be infinite if None
+        iterator = iter(int, 1) if iterator is None else iterator.__iter__()
 
-    for i in iterator:
+    while True:
+        try:
+            i = (await iterator.__anext__()) if is_async else iterator.__next__()
+        except (StopIteration, StopAsyncIteration):
+            break
         await asyncio.sleep(max(1e-3, 1./rate + t - time.perf_counter()))
         t = time.perf_counter()
         update_func()
