ab537560d9fbccd5159b8df22ddd1372f6154094
John-Henry Lim <42513874+Interpause@users.noreply.github.com>
2022-05-04 17:31:58 +0800

    proof of concept

--- nicepipe/__main__.py
+++ nicepipe/__main__.py
@@ -1,13 +1,17 @@
 import asyncio
+import dearpygui.dearpygui as dpg
 from nicepipe.utils import enable_fancy_console
 
-from .gui import init_gui
+from .gui import create_gui
 
 # import uvicorn
 
 
 async def main():
     # uvicorn.run(app, host="127.0.0.1", port=5000, log_level="info")
+    with create_gui():
+        while dpg.is_dearpygui_running():
+            dpg.render_dearpygui_frame()
     pass
 
 
--- nicepipe/gui/__init__.py
+++ nicepipe/gui/__init__.py
@@ -1,7 +1,24 @@
 import logging
+import dearpygui.dearpygui as dpg
+from contextlib import contextmanager
 
 log = logging.getLogger(__name__)
 
 
-def init_gui(argv=[]):
-    pass
+@contextmanager
+def create_gui():
+    dpg.create_context()
+    with dpg.window(label="Example"):
+        dpg.add_text("hello world")
+        dpg.add_button(label="save")
+        dpg.add_input_text(label="string", default_value="placeholder")
+        dpg.add_slider_float(label="float", default_value=0.5, max_value=1)
+
+    dpg.create_viewport(title="dumb", width=600, height=400)
+    dpg.setup_dearpygui()
+    dpg.show_viewport()
+
+    try:
+        yield
+    finally:
+        dpg.render_dearpygui_frame()
